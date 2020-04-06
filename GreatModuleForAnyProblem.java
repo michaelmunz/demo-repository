532619bba33d683413e81a928eb43f3d7a843cbc
Michael Munz <michael.munz@hs-ulm.de>
2020-04-06 11:23:35 +0200

    klassenkopf hinzugefügt

--- GreatModuleForAnyProblem.java
+++ GreatModuleForAnyProblem.java
@@ -1,7 +1,6 @@
 // This is a sourcecode module which solves any problem provided by the user
 
-public static void main(String[] args){
-	//TODO: implement
+public class GreatModuleForAnyProblem{
 	public static void main(String[] args){
 		System.out.println("Dies ist die Test-Ausgabe");
 	}
